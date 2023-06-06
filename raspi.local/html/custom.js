8db9ba50ac0b179b4f4b4ab30ee2df9c5cdd84c2
Sam Gleaves <sam@gleaves.studio>
2023-06-06 10:43:33 +0100

    Sideboard

--- raspi.local/html/custom.js
+++ raspi.local/html/custom.js
@@ -1,17 +1,29 @@
-jQuery(document).ready(function(){
-    var apps = [
-        {name:"Prowlarr", port:9696},
-        {name:"Sonarr", port:8989},
-        {name:"Bazarr", port:6767},
-        {name:"Radarr", port:7878},
-        {name:"Jellyseerr", port:5055},
-        {name:"Jellyfin", port:8096},
-        {name:"Transmission", port:9091}
-    ];
-    var url = window.location.host;
-    
-    apps.forEach(element => {
-        var btn = '<a target="_blank" href="http://'+url+':'+element.port+'"><button type="button" class="btn btn-dark m-2">'+element.name+'</button></a>';
-        jQuery('#container').append(btn);
-    });
+jQuery(document).ready(function () {
+  var apps = [
+    { name: 'Prowlarr', port: 9696 },
+    { name: 'Sonarr', port: 8989 },
+    { name: 'Bazarr', port: 6767 },
+    { name: 'Radarr', port: 7878 },
+    { name: 'Jellyseerr', port: 5055 },
+    { name: 'Jellyfin', port: 8096 },
+    { name: 'Transmission', port: 9091 },
+    { name: 'QNAP', port: 8080, ip: '192.168.1.8' },
+  ];
+  var url = window.location.hostname;
+
+  apps.forEach((element) => {
+    var host = url;
+    if (element.ip) host = element.ip;
+    var btn =
+      '<li class="nav-item"><a id="' +
+      element.name +
+      '" class="nav-link" target="_blank" data-href="http://' +
+      host +
+      ':' +
+      element.port +
+      '">' +
+      element.name +
+      '</a></li>';
+    jQuery('#container').append(btn);
+  });
 });
